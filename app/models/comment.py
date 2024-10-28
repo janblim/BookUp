8e73e4244850b6540569f3d9a16aa5fe163c0238
Jan Lim <jan.borja.lim@gmail.com>
2024-10-28 10:49:11 -0700

    removed foreign key association from parent comment, re-added ups and replies in comments

--- app/models/comment.py
+++ app/models/comment.py
@@ -12,13 +12,13 @@ class Comment(db.Model):
     text = db.Column(db.Text)
     user_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod('users.id'), ondelete='CASCADE'), nullable=False)
     post_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod('posts.id'), ondelete='CASCADE'), nullable=False)
-    parent_comment_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod('comments.id'), ondelete='CASCADE'), nullable=True)
+    parent_comment_id = db.Column(db.Integer)
     score = db.Column(db.Integer, default=0)
     created_at = db.Column(db.DateTime(timezone=True), default=datetime.now(), nullable=False)
     updated_at = db.Column(db.DateTime(timezone=True), default=datetime.now(), nullable=False)
 
-    # ups = db.relationship('Up', backref='comment', lazy=True)
-    # replies = db.relationship('Comment', backref='parent', remote_side=[id], lazy=True)
+    ups = db.relationship('Up', backref='comment', lazy=True)
+    replies = db.relationship('Comment', backref='parent', remote_side=[id], lazy=True)
 
     def to_dict(self):
         return {
@@ -28,10 +28,10 @@ class Comment(db.Model):
             'text': self.text,
             'parent_comment_id': self.parent_comment_id,
             'score': self.score,
-            # 'ups': [{
-            #     'user_id': up.user_id,
-            #     'value': up.value
-            # } for up in self.ups],
+            'ups': [{
+                'user_id': up.user_id,
+                'value': up.value
+            } for up in self.ups],
             'created_at': self.created_at,
             'updated_at': self.updated_at
         }
