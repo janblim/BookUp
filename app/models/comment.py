774cf55c9113fda2444da0430175558143e14d8f
Jan Lim <jan.borja.lim@gmail.com>
2024-11-08 16:15:27 -0800

    removed extra _ in __table_args__ in comments.py. If this was seriously the only problem immacutta

--- app/models/comment.py
+++ app/models/comment.py
@@ -6,7 +6,7 @@ class Comment(db.Model):
     __tablename__ = 'comments'
 
     if environment == 'production':
-        __table__args__ = {'schema': SCHEMA}
+        __table_args__ = {'schema': SCHEMA}
 
     id = db.Column(db.Integer, primary_key=True)
     text = db.Column(db.Text)
